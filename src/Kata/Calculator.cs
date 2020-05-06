aa57ddf1651f3c05b772382f398d725b408f1602
Josue Salinas <saltt17@gmail.com>
2020-05-05 22:48:02 -0600

    when input has a negative value

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -21,6 +21,12 @@ public class Calculator
         }
 
         var elements = s.Split(delimiters,StringSplitOptions.None).Select(int.Parse);
+
+        if (elements.Any(x=> x <0) )
+        {
+            throw new Exception("negatives not allowed: -1");
+        }
+        
         return elements.Sum();
     }
 }
