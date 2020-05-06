05313f4e81f24e0c27812b22360ef5d15207bc30
Josue Salinas <saltt17@gmail.com>
2020-05-05 22:36:51 -0600

    number custom delimeter test and pass

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -5,12 +5,22 @@ public class Calculator
 {
     public int Add(string s = "")
     {
+        
         if (string.IsNullOrEmpty(s))
         {
             return 0;            
         }
 
-        var elements = s.Split(new []{"\n",","},StringSplitOptions.None).Select(int.Parse);
+        var delimiters = new[] {"\n", ","};
+        if (s.Contains("//"))
+        {
+            var splitText = s.Split("\n");
+            
+            delimiters = new[] {splitText[0].Replace("/", "")};
+            s = splitText[1];
+        }
+
+        var elements = s.Split(delimiters,StringSplitOptions.None).Select(int.Parse);
         return elements.Sum();
     }
 }
